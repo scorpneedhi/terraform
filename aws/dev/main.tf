ff41273eab57c4fcdb1a615abf981df0000fb3fa
Needhi Devan Senthil Kumar <needhi.senthil@gmail.com>
2023-02-15 20:57:29 -0700

    configured elb and autoscaling on dev

--- aws/dev/main.tf
+++ aws/dev/main.tf
@@ -4,17 +4,23 @@ module "instance_provisioning" {
 
   aws_region = var.aws_region
 
+  vpc_cidr = var.vpc_cidr
+
+  subnets_cidr = var.subnets_cidr
+
+  azs= var.azs
+
   aws_instance_ami = var.aws_instance_ami
 
-  aws_instance_instance_type = var.aws_instance_instance_type
+  aws_instance_type = var.aws_instance_type
 
   aws_instance_key_name = var.aws_instance_key_name
 
-  aws_instance_tags = var.aws_instance_tags
+  aws_launch_configuration_name = var.aws_launch_configuration_name
 
-  aws_ebs_volume_size = var.aws_ebs_volume_size
-
-  aws_ebs_volume_tags = var.aws_ebs_volume_tags
-
-  aws_volume_attachment_device_name = var.aws_volume_attachment_device_name
+#  aws_ebs_volume_size = var.aws_ebs_volume_size
+#
+#  aws_ebs_volume_tags = var.aws_ebs_volume_tags
+#
+#  aws_volume_attachment_device_name = var.aws_volume_attachment_device_name
 }
--- aws/dev/terraform.tfvars
+++ aws/dev/terraform.tfvars
@@ -1,15 +1,13 @@
 aws_region = "us-west-1"
 
-aws_instance_ami = "ami-0036b4598ccd42565"
+aws_instance_ami = "ami-00569e54da628d17c"
 
-aws_instance_instance_type = "t2.micro"
+aws_instance_type = "t2.micro"
 
 aws_instance_key_name ="devops-kp"
 
-aws_instance_tags="webserver1"
-
-aws_ebs_volume_tags="webserver1_volume"
-
-aws_ebs_volume_size=10
-
-aws_volume_attachment_device_name="/dev/sdh"
+#aws_ebs_volume_tags="webserver1_volume"
+#
+#aws_ebs_volume_size=10
+#
+#aws_volume_attachment_device_name="/dev/sdh"
